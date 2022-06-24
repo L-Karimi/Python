fac74e4c7ef9130f55f5f1b118214deb2f42e196
L-Karimi <luciahkei101@gmail.com>
2022-06-24 12:47:59 +0300

    modified file

--- two_pointer_fun.py
+++ two_pointer_fun.py
@@ -1,18 +1,21 @@
 # write a program that reverses a list
-sentense="I am AkiraChix"
-new_sentence= sentense.split()
-print(new_sentence)
 
-start=0
-end=len(new_sentence)-1
-while start<end:
-    new_sentence[start],end= new_sentence[end],start
-    start +=1
-    end-=1
-    new_string=""
+def reverse_string():
+    sentense="I am AkiraChix"
+    new_sentence= sentense.split()
     print(new_sentence)
-    new=new_sentence.append(new_string)
-    print(new)
-    print(new_string)
-    print(new_sentence)
+    start=0
+    end=len(new_sentence)-1
+    while start<end:
+        new_sentence[start],end= new_sentence[end],start
+        
+        start +=1
+        end-=1
+        new_string=""
+        print(new_sentence)
+        new=new_sentence.append(new_string)
+        print(new)
+        print(new_string)
+        print(new_sentence)
+reverse_string()
     

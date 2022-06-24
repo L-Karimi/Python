60eea8461a0f846e50b998f137f8f2d1bbbd1b6c
L-Karimi <luciahkei101@gmail.com>
2022-06-24 16:27:15 +0300

    modified file

--- two_pointer_fun.py
+++ two_pointer_fun.py
@@ -1,22 +1,31 @@
 # write a program that reverses a list
-sentense="I am AkiraChix"
 
-def reverse_string():
-    new_sentence= sentense.split()
-    print(new_sentence)
+
+sentence="I am AkiraChix"
+
+def reverse_string(sentence):
+    new_sentence= sentence.split()
     start=0
     end=len(new_sentence)-1
-    while start<end:
-        new_sentence[start],end= new_sentence[end],start
+    while start<=end:
+        new_sentence[start],new_sentence[end]= new_sentence[end],new_sentence[start]
         start +=1
         end-=1
-        new_string=""
-        print(new_sentence)
-        new=new_sentence.append(new_string)
-        print(str(new))
+        new_string=" ".join(new_sentence)
         print(new_string)
-        print(new_sentence)
-reverse_string()
+reverse_string("I am AkiraChix")
 
 
+# s="I am AkiraChix"
+# def reverseString( ) :
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         i = 0
+#         j = len(s) - 1
+#         while i <= j:
+#             s[i] , s[j] = s[j] , s[i]
+#             i += 1
+#             j -= 1
+# reverseString()
     

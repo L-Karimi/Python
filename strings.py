7d185bd2623274f10a268987cc40f952ed43dad4
L-Karimi <luciahkei101@gmail.com>
2022-06-24 12:05:49 +0300

    added two pointer fun file

--- strings.py
+++ strings.py
@@ -1,3 +1,4 @@
+from curses.ascii import isupper
 from unittest import result
 
 
@@ -31,3 +32,29 @@ print(my_homeplace.find("come"))
 for word in my_homeplace:
     print(word.isdigit())
     break
+
+
+full_name = "Lucy Karimi"
+y=len(full_name)//2
+name=""
+# x1=0
+# x2=0
+# for x in full_name:
+#     if (x1.isLower()):
+#         print(x.count())
+#     elif(i.isupper()):
+#         print(x.count())
+for i in enumerate(full_name):
+    print(i)
+students={"Lucy":22,"Ian":25,"Jebitok":33,"Alex":12,"Annita":24}
+for i in enumerate(students):
+    print(i)
+schools=["Co-operative","AkiraChix","Nairobi Uni","Kenyatta","Maseno"]
+courses=["Business management","Information technology","computer science","Acturial science","Business information technology"]
+for x,y in zip(schools,courses):
+    print(x,y)
+characters=["r","t","y","q","d","s","J","v","n"]
+new_chars=characters[:]
+print(new_chars)
+characters[1:5] =[]
+print()
---
+++ two_pointer_fun.py
@@ -0,0 +1,18 @@
+# write a program that reverses a list
+sentense="I am AkiraChix"
+new_sentence= sentense.split()
+print(new_sentence)
+
+start=0
+end=len(new_sentence)-1
+while start<end:
+    new_sentence[start],end= new_sentence[end],start
+    start +=1
+    end-=1
+    new_string=""
+    print(new_sentence)
+    new=new_sentence.append(new_string)
+    print(new)
+    print(new_string)
+    print(new_sentence)
+    

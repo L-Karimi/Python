a9ba87938935c4f129467923afdc67089ff776ae
L-Karimi <luciahkei101@gmail.com>
2022-06-27 10:34:30 +0300

    palindrome

--- two_pointer_fun.py
+++ two_pointer_fun.py
@@ -1,6 +1,9 @@
 # write a program that reverses a list
 
 
+import string
+
+
 sentence="I am AkiraChix"
 
 def reverse_string(sentence):
@@ -15,6 +18,31 @@ def reverse_string(sentence):
         print(new_string)
 reverse_string("I am AkiraChix")
 
+def int_palin():
+    num=int(input("Enter a number:"))
+    x=num
+    s=0
+    while(num>0):
+        result=num%10
+        s=s*10+result
+        num=num//10
+    if(x==s):
+        print("The number is palindrome")
+    else:
+        print("Not a palindrome!")
+int_palin()
+
+s=string(input("Please ender word:"))
+def palindrome(s):
+    start=0
+    end=len(s)-1
+    while start<=end:
+        s[start],s[end]=s[end],s[start]
+        if s==s:
+            print("This is a plaindrome")
+        else:
+            print("This is not a plaindrome")
+palindrome("Enter a word:")
 
 
     
